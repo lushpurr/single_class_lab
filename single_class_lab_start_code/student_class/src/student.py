1f0f788e8511e0a0298064ce74a030c595f70bf0
lushpurr <lushpurr@gmail.com>
2020-10-19 14:28:42 +0100

    inital commit

---
+++ single_class_lab_start_code/student_class/src/student.py
@@ -0,0 +1,16 @@
+# Part A
+# CodeClan student.
+
+# The files for this section can be found in single_class_lab_start_code/student_class
+
+# You should write your code in the the src/student.py file. The tests we have provided are in tests/student_test.py They are all skipped at the moment. You should work through the tests one at a time, deleting the line which causes the test to skip.
+
+# To run the tests:
+
+# python3 run_tests.py
+
+# Tasks
+# Create a class called Student that has the properties Name (str) and Cohort (str).
+# Create an __init__ method that takes in a name (str) and a cohort (str - e.g "E41", "G19", etc) to initialise the properties when a new student is created.
+# Create a talk method that gets the student to talk, returning "I can talk!".
+# Create a method, say_favourite_language that takes in a students favourite programming language and returns it as part of a string (eg. student.say_favourite_language("Python") -> "I love Python").
